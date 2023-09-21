f47c6dfb2d37a56d59f18aa490909d9db610beaf
saddamhossen4505 <seoexpartsaddamhossen@gmail.com>
2023-09-21 11:17:26 +0600

    update all

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,9 @@ import "./index.css";
 import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  
 );
